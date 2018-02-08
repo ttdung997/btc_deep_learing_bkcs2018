906123ff2179a307a4ba578b9e003761ebbaf0c0
mrhieusd <mrhieusd@gmail.com>
2018-02-08 15:54:26 +0700

    erroe model

--- v2/program.py
+++ v2/program.py
@@ -17,15 +17,15 @@ from scipy import interp
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
-basePath="/home/bkcs/Desktop/dung/"
+basePath="/home/bkcs/Desktop/dung/v2/"
 eth_Close_model_id = 1
 eth_Volume_model_id = 2
-eth_close_off_high_model_id =3
-eth_volatility_model_id = 4
+eth_High_model_id =3
+eth_Low_model_id = 4
 bt_Close_model_id = 5
 bt_Volume_model_id = 6
-bt_close_off_high_model_id = 7
-bt_volatility_model_id = 8
+bt_High_model_id = 7
+bt_Low_model_id= 8
 class BtcPredict():
     def _init_(self):
         print("This is bitcoin predict!")
@@ -43,14 +43,14 @@ class BtcPredict():
             self.eth_Volume_model = model_from_json(loaded_model_json)
             # load weights into new model
             self.eth_Volume_model.load_weights(weight) 
-        elif(id==eth_close_off_high_model_id):
-            self.eth_close_off_high_model = model_from_json(loaded_model_json)
+        elif(id==eth_High_model_id):
+            self.eth_High_model = model_from_json(loaded_model_json)
             # load weights into new model
-            self.eth_close_off_high_model.load_weights(weight)
-        elif(id== eth_volatility_model_id):
-            self.eth_volatility_model = model_from_json(loaded_model_json)
+            self.eth_High_model.load_weights(weight) 
+        elif(id==eth_Low_model_id):
+            self.eth_Low_model = model_from_json(loaded_model_json)
             # load weights into new model
-            self.eth_volatility_model.load_weights(weight) 
+            self.eth_Low_model.load_weights(weight)
         elif(id== bt_Close_model_id):
             self.bt_Close_model = model_from_json(loaded_model_json)
             # load weights into new model
@@ -59,33 +59,34 @@ class BtcPredict():
             self.bt_Volume_model = model_from_json(loaded_model_json)
             # load weights into new model
             self.bt_Volume_model.load_weights(weight) 
-        elif(id==bt_close_off_high_model_id):
-            self.bt_close_off_high_model = model_from_json(loaded_model_json)
+        # print("Loaded model from disk")
+        elif(id== bt_High_model_id):
+            self.bt_High_model = model_from_json(loaded_model_json)
             # load weights into new model
-            self.bt_close_off_high_model.load_weights(weight)
-        elif(id== bt_volatility_model_id):
-            self.bt_volatility_model = model_from_json(loaded_model_json)
+            self.bt_High_model.load_weights(weight) 
+        # print("Loaded model from disk")
+        elif(id== bt_Low_model_id):
+            self.bt_Low_model = model_from_json(loaded_model_json)
             # load weights into new model
-            self.bt_volatility_model.load_weights(weight) 
-
+            self.bt_Low_model.load_weights(weight) 
         # print("Loaded model from disk")
     def predict(self,data,id):
         if(id == eth_Close_model_id):
             return self.eth_Close_model.predict(data)
         elif(id==eth_Volume_model_id):
             return self.eth_Volume_model.predict(data)
-        elif(id==eth_close_off_high_model_id):
-            return self.eth_close_off_high_model.predict(data)
-        elif(id==eth_volatility_model_id):
-            return self.eth_volatility_model.predict(data)
+        elif(id==eth_High_model_id):
+            return self.eth_High_model.predict(data)
+        elif(id==eth_Low_model_id):
+            return self.eth_Low_model.predict(data)
         elif(id==bt_Close_model_id):
             return self.bt_Close_model.predict(data)
         elif(id==bt_Volume_model_id):
             return self.bt_Volume_model.predict(data)
-        elif(id==bt_close_off_high_model_id):
-            return self.bt_close_off_high_model.predict(data)
-        elif(id==bt_volatility_model_id):
-            return self.bt_volatility_model.predict(data)
+        elif(id==bt_High_model_id):
+            return self.bt_High_model.predict(data)
+        elif(id==bt_Low_model_id):
+            return self.bt_Low_model.predict(data)
 
 if __name__ == "__main__":
     eth_Close_model = basePath+"eth_Close_model.json"
@@ -94,11 +95,12 @@ if __name__ == "__main__":
     eth_Volume_model = basePath+"eth_Volume_model.json"
     eth_Volume_model_weight = basePath+"eth_Volume_model.h5"
 
-    eth_close_off_high_model = basePath+"eth_close_off_high_model.json"
-    eth_close_off_high_model_weight = basePath+"eth_close_off_high_model.h5"
 
-    eth_volatility_model = basePath+"eth_volatility_model.json"
-    eth_volatility_model_weight = basePath+"eth_volatility_model.h5"
+    eth_High_model = basePath+"eth_High_model.json"
+    eth_High_model_weight = basePath+"eth_High_model.h5"
+
+    eth_Low_model = basePath+"eth_Low_model.json"
+    eth_Low_model_weight = basePath+"eth_Low_model.h5"
 
     bt_Close_model = basePath+"bt_Close_model.json"
     bt_Close_model_weight = basePath+"bt_Close_model.h5"
@@ -106,65 +108,74 @@ if __name__ == "__main__":
     bt_Volume_model = basePath+"bt_Volume_model.json"
     bt_Volume_model_weight = basePath+"bt_Volume_model.h5"
 
-    bt_close_off_high_model = basePath+"bt_close_off_high_model.json"
-    bt_close_off_high_model_weight = basePath+"bt_close_off_high_model.h5"
+    bt_High_model = basePath+"bt_High_model.json"
+    bt_High_model_weight = basePath+"bt_High_model.h5"
 
-    bt_volatility_model = basePath+"bt_volatility_model.json"
-    bt_volatility_model_weight = basePath+"bt_volatility_model.h5"
+    bt_Low_model = basePath+"bt_Low_model.json"
+    bt_Low_model_weight = basePath+"bt_Low_model.h5"
+
+
 
     btc_predict =BtcPredict()
 
     btc_predict.loadModel(eth_Close_model,eth_Close_model_weight,eth_Close_model_id)
     btc_predict.loadModel(eth_Volume_model,eth_Volume_model_weight,eth_Volume_model_id)
-    btc_predict.loadModel(eth_close_off_high_model,eth_close_off_high_model_weight,eth_close_off_high_model_id)
-    btc_predict.loadModel(eth_volatility_model,eth_volatility_model_weight,eth_volatility_model_id)
+    btc_predict.loadModel(eth_High_model,eth_High_model_weight,eth_High_model_id)
+    btc_predict.loadModel(eth_Low_model,eth_Low_model_weight,eth_Low_model_id)
     btc_predict.loadModel(bt_Close_model,bt_Close_model_weight,bt_Close_model_id)
     btc_predict.loadModel(bt_Volume_model,bt_Volume_model_weight,bt_Volume_model_id)
-    btc_predict.loadModel(bt_close_off_high_model,bt_close_off_high_model_weight,bt_close_off_high_model_id)
-    btc_predict.loadModel(bt_volatility_model,bt_volatility_model_weight,bt_volatility_model_id)
+    btc_predict.loadModel(bt_High_model,bt_High_model_weight,bt_High_model_id)
+    btc_predict.loadModel(bt_Low_model,bt_Low_model_weight,bt_Low_model_id)
 
     data = pd.read_csv("lastdata.csv")
     data = np.array(data)
-    data.shape = (1,50,8)
-    print(data)
-    window_len = 10
+    data.shape = (1,20,8)
+    # print(data)
+    window_len = 20
     predict_date=pd.read_csv('date.csv')
 
     test_set = pd.read_csv("test_set.csv")
+
+    # print(test_set['bt_Close'].values[:-window_len])
     predict = []
     predict_price=[]
     i=0
-    while i<181:
+    while i<171:
         eth_Close_predict = btc_predict.predict(data,eth_Close_model_id)
         eth_Volume_predict = btc_predict.predict(data,eth_Volume_model_id)
-        eth_close_off_high_predict = btc_predict.predict(data,eth_close_off_high_model_id)
-        eth_volatility_predict = btc_predict.predict(data,eth_volatility_model_id)
+        eth_High_predict = btc_predict.predict(data,eth_High_model_id)
+        eth_Low_predict = btc_predict.predict(data,eth_Low_model_id)
         bt_Close_predict = btc_predict.predict(data,bt_Close_model_id)
         bt_Volume_predict = btc_predict.predict(data,bt_Volume_model_id)
-        bt_close_off_high_predict = btc_predict.predict(data,bt_close_off_high_model_id)
-        bt_volatility_predict = btc_predict.predict(data,bt_volatility_model_id)
-        eth_price_predict = ((np.transpose(eth_Close_predict)+1) * test_set['eth_Close'].values[:-window_len])[0][-1]
-        predict.append([eth_Close_predict[0][0]])
-        predict_price.append(eth_price_predict)
-        test_set['eth_Close'].values[:-window_len][-1] = eth_price_predict
+        bt_High_predict = btc_predict.predict(data,bt_High_model_id)
+        bt_Low_predict = btc_predict.predict(data,bt_Low_model_id)
+        # bt_volatility_predict = btc_predict.predict(data,bt_volatility_model_id)
+        bt_price_predict = ((np.transpose(bt_Close_predict)+0.9) * test_set['bt_Close'].values[:-window_len])[0][-1]
+        predict.append([bt_Close_predict[0][0]])
+        predict_price.append(bt_price_predict)
+        # test_set['bt_Close'].values[:-window_len][-1] = bt_price_predict
         data= np.delete(data[0], 0,0)
-        last_row = np.array([eth_Close_predict[0][0], eth_Volume_predict[0][0] 
-                            , eth_close_off_high_predict[0][0] , eth_volatility_predict[0][0] 
+        last_row = np.array([eth_Close_predict[0][0], eth_Volume_predict[0][0]
+                            , eth_High_predict[0][0] , eth_Low_predict[0][0]
                             , bt_Close_predict[0][0] , bt_Volume_predict[0][0] 
-                            , bt_close_off_high_predict[0][0] , bt_volatility_predict[0][0] ])
+                            , bt_High_predict[0][0] , bt_Low_predict[0][0] ])
         data = np.concatenate((data, [last_row]), axis=0)
-        data.shape = (1,50,8)
+        data.shape = (1,20,8)
         i = i+1
     print(predict)
 
     print(predict_price)
+
+
+    
+    # print()
 
     fig, ax1 = plt.subplots(1,1)
     ax1.set_xticks([datetime.date(2018,i+1,1) for i in range(12)])
     ax1.set_xticklabels([datetime.date(2018,i+1,1).strftime('%b %d %Y')  for i in range(12)])
     # ax1.plot(model_data[model_data['Date']>= split_date]['Date'][window_len:].astype(datetime.datetime),
     #          test_set['eth_Close'][window_len:], label='Actual')
-    ax1.plot(predict_date.head(181)['date'].astype(datetime.datetime),
+    ax1.plot(predict_date.head(171)['date'].astype(datetime.datetime),
              predict_price, label='Predicted')
     ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(predict)+1)-\
                 (test_set['eth_Close'].values[window_len:])/(test_set['eth_Close'].values[:-window_len]))), 
--- v2/train.py
+++ v2/train.py
@@ -86,11 +86,16 @@ for coins in ['bt_', 'eth_']:
 
 
 model_data = market_info[['Date']+[coin+metric for coin in ['bt_', 'eth_'] 
-                                   for metric in ['Close','Volume','close_off_high','volatility']]]
+                                   for metric in ['Close','Volume','close_off_high','volatility','High','Low']]]
 # need to reverse the data frame so that subsequent rows represent later timepoints
 model_data = model_data.sort_values(by='Date')
+
+model_data = model_data.drop('eth_close_off_high',1)
+model_data = model_data.drop('bt_close_off_high',1)
+model_data = model_data.drop('eth_volatility',1)
+model_data = model_data.drop('bt_volatility',1)
+
 print (model_data.head())
-# print(model_data.head())
 
 # we don't need the date columns anymore
 training_set, test_set = model_data[model_data['Date']<split_date], model_data[model_data['Date']>=split_date]
@@ -99,7 +104,7 @@ test_set = test_set.drop('Date', 1)
 split_date2 = '2017-08-01'
 model_data[model_data['Date']>=split_date2].to_csv("test_set.csv")
 print("created test set to csv")
-window_len = 50
+window_len = 20
 norm_cols = [coin+metric for coin in ['bt_', 'eth_'] for metric in ['Close','Volume']]
 
 # print(len(model_data[model_data['Date']>= split_date]['Date'][window_len:].astype(datetime.datetime)))
@@ -128,13 +133,15 @@ LSTM_test_outputs = (test_set['eth_Close'][window_len:].values/test_set['eth_Clo
 
 LSTM_training_inputs = [np.array(LSTM_training_input) for LSTM_training_input in LSTM_training_inputs]
 LSTM_training_inputs = np.array(LSTM_training_inputs)
- # print(LSTM_training_inputs)
+print(LSTM_training_inputs)
+print(LSTM_training_inputs.shape[1])
+print(LSTM_training_inputs.shape[2])
 LSTM_test_inputs = [np.array(LSTM_test_inputs) for LSTM_test_inputs in LSTM_test_inputs]
 LSTM_test_inputs = np.array(LSTM_test_inputs)
 LSTM_last_input.to_csv("lastdata2.csv")
 print("created csv data file")
 LSTM_last_input = LSTM_test_inputs[-1]
-LSTM_last_input.shape = (1,50,8)
+LSTM_last_input.shape = (1,20,8)
 
 np.random.seed(202)
 
@@ -144,50 +151,50 @@ np.random.seed(202)
 # LSTM_training_outputs = (training_set['eth_Close'][window_len:].values/training_set['eth_Close'][:-window_len].values)-1
 # note: eth_history contains information on the training error per epoch
 # eth_history = eth_model.fit(LSTM_training_inputs, LSTM_training_outputs, 
-#                             epochs=10, batch_size=1, verbose=2, shuffle=True)
+#                             epochs=20, batch_size=1, verbose=2, shuffle=True)
 print("this is eth_Close model")
 LSTM_training_eth_Close_outputs = (training_set['eth_Close'][window_len:].values/training_set['eth_Close'][:-window_len].values)-1
 eth_Close_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
 eth_Close_model.fit(LSTM_training_inputs, LSTM_training_eth_Close_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
 print("this is eth_Volume model")
 LSTM_training_eth_Volume_outputs = (training_set['eth_Volume'][window_len:].values/training_set['eth_Volume'][:-window_len].values)-1
-eth_Volume_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
+eth_Volume_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
 eth_Volume_model.fit(LSTM_training_inputs, LSTM_training_eth_Volume_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
-LSTM_training_eth_close_off_high_outputs = (training_set['eth_close_off_high'][window_len:].values/training_set['eth_close_off_high'][:-window_len].values)-1
-eth_close_off_high_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
-eth_history = eth_close_off_high_model.fit(LSTM_training_inputs, LSTM_training_eth_close_off_high_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
-print("this is eth_volatility model")
-LSTM_training_eth_volatility_outputs = (training_set['eth_volatility'][window_len:].values/training_set['eth_volatility'][:-window_len].values)-1
-eth_volatility_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
-eth_volatility_model.fit(LSTM_training_inputs, LSTM_training_eth_volatility_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+LSTM_training_eth_High_outputs = (training_set['eth_High'][window_len:].values/training_set['eth_High'][:-window_len].values)-1
+eth_High_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
+eth_history = eth_High_model.fit(LSTM_training_inputs, LSTM_training_eth_High_outputs, 
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
+print("this is eth_Low model")
+LSTM_training_eth_Low_outputs = (training_set['eth_Low'][window_len:].values/training_set['eth_Low'][:-window_len].values)-1
+eth_Low_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
+eth_Low_model.fit(LSTM_training_inputs, LSTM_training_eth_Low_outputs, 
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
 print("this is bt_Close model")
 LSTM_training_bt_Close_outputs = (training_set['bt_Close'][window_len:].values/training_set['bt_Close'][:-window_len].values)-1
-bt_Close_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
+bt_Close_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
 bt_Close_model.fit(LSTM_training_inputs, LSTM_training_bt_Close_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
 print("this is bt_Volume model")
 LSTM_training_bt_Volume_outputs = (training_set['bt_Volume'][window_len:].values/training_set['bt_Volume'][:-window_len].values)-1
-bt_Volume_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
+bt_Volume_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
 bt_Volume_model.fit(LSTM_training_inputs, LSTM_training_bt_Volume_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
-LSTM_training_bt_close_off_high_outputs = (training_set['bt_close_off_high'][window_len:].values/training_set['bt_close_off_high'][:-window_len].values)-1
-bt_close_off_high_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
-bt_history = bt_close_off_high_model.fit(LSTM_training_inputs, LSTM_training_bt_close_off_high_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
-print("this is bt_volatility model")
-LSTM_training_bt_volatility_outputs = (training_set['bt_volatility'][window_len:].values/training_set['bt_volatility'][:-window_len].values)-1
-bt_volatility_model =build_model(LSTM_training_inputs, output_size=1, neurons = 20)
-bt_volatility_model.fit(LSTM_training_inputs, LSTM_training_bt_volatility_outputs, 
-                            epochs=10, batch_size=1, verbose=2, shuffle=True)
+LSTM_training_bt_High_outputs = (training_set['bt_High'][window_len:].values/training_set['bt_High'][:-window_len].values)-1
+bt_High_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
+bt_history = bt_High_model.fit(LSTM_training_inputs, LSTM_training_bt_High_outputs, 
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
+print("this is bt_Low model")
+LSTM_training_bt_Low_outputs = (training_set['bt_Low'][window_len:].values/training_set['bt_Low'][:-window_len].values)-1
+bt_Low_model =build_model(LSTM_training_inputs, output_size=1, neurons = 30)
+bt_Low_model.fit(LSTM_training_inputs, LSTM_training_bt_Low_outputs, 
+                            epochs=20, batch_size=1, verbose=2, shuffle=True)
 
 
 model_json = eth_Close_model.to_json()
@@ -206,21 +213,21 @@ with open(model_output, "w") as json_file:
         # serialize weights to HDF5
         eth_Volume_model.save_weights(weight_output)
 
-model_json = eth_close_off_high_model.to_json()
-model_output = "eth_close_off_high_model.json"
-weight_output = "eth_close_off_high_model.h5"
+model_json = eth_High_model.to_json()
+model_output = "eth_High_model.json"
+weight_output = "eth_High_model.h5"
 with open(model_output, "w") as json_file:
         json_file.write(model_json)
         # serialize weights to HDF5
-        eth_close_off_high_model.save_weights(weight_output)
+        eth_High_model.save_weights(weight_output)
 
-model_json = eth_volatility_model.to_json()
-model_output = "eth_volatility_model.json"
-weight_output = "eth_volatility_model.h5"
+model_json = eth_Low_model.to_json()
+model_output = "eth_Low_model.json"
+weight_output = "eth_Low_model.h5"
 with open(model_output, "w") as json_file:
         json_file.write(model_json)
         # serialize weights to HDF5
-        eth_volatility_model.save_weights(weight_output)
+        eth_Low_model.save_weights(weight_output)
 
 model_json = bt_Close_model.to_json()
 model_output = "bt_Close_model.json"
@@ -238,99 +245,99 @@ with open(model_output, "w") as json_file:
         # serialize weights to HDF5
         bt_Volume_model.save_weights(weight_output)
 
-model_json = bt_close_off_high_model.to_json()
-model_output = "bt_close_off_high_model.json"
-weight_output = "bt_close_off_high_model.h5"
+model_json = bt_High_model.to_json()
+model_output = "bt_High_model.json"
+weight_output = "bt_High_model.h5"
 with open(model_output, "w") as json_file:
         json_file.write(model_json)
         # serialize weights to HDF5
-        bt_close_off_high_model.save_weights(weight_output)
+        bt_High_model.save_weights(weight_output)
 
-model_json = bt_volatility_model.to_json()
-model_output = "bt_volatility_model.json"
-weight_output = "bt_volatility_model.h5"
+model_json = bt_Low_model.to_json()
+model_output = "bt_Low_model.json"
+weight_output = "bt_Low_model.h5"
 with open(model_output, "w") as json_file:
         json_file.write(model_json)
         # serialize weights to HDF5
-        bt_volatility_model.save_weights(weight_output)
-
+        bt_Low_model.save_weights(weight_output)
 
 
 print("Saved model to disk")
 
 
 
-New_input = LSTM_last_input
+# New_input = LSTM_last_input
+# # print(New_input)
+# # print(New_input[0][9])
+# New_eth_Close =eth_Close_model.predict(LSTM_last_input)
+
+# New_eth_Volume =eth_Volume_model.predict(LSTM_last_input)
+
+# # New_eth_close_off_high =eth_close_off_high_model.predict(LSTM_last_input)
+
+# New_eth_volatilityeth =eth_volatility_model.predict(LSTM_last_input)
+
+# New_bt_Close =bt_Close_model.predict(LSTM_last_input)
+
+# New_bt_Volume =bt_Volume_model.predict(LSTM_last_input)
+
+# # New_bt_close_off_high =bt_close_off_high_model.predict(LSTM_last_input)
+
+# New_bt_volatilitybt_close =bt_volatility_model.predict(LSTM_last_input)
+
+# New_input[0][9][0] = New_eth_Close
+
+# New_input[0][9][1] = New_eth_Volume
+
+# # New_input[0][9][2] = eth_close_off_high_model.predict(LSTM_last_input)
+
+
+# # New_input[0][9][2] = New_eth_volatilityeth
+
+# New_input[0][9][2] = New_bt_Close
+
+# New_input[0][9][3] = New_bt_Volume
+
+# # New_input[0][9][6] = bt_close_off_high_model.predict(LSTM_last_input)
+
+
+# # New_input[0][9][5] =New_bt_volatilitybt_close
+
+# # print(New_input)
 # print(New_input)
-# print(New_input[0][9])
-New_eth_Close =eth_Close_model.predict(LSTM_last_input)
-
-New_eth_Volume =eth_Volume_model.predict(LSTM_last_input)
-
-# New_eth_close_off_high =eth_close_off_high_model.predict(LSTM_last_input)
-
-New_eth_volatilityeth =eth_volatility_model.predict(LSTM_last_input)
-
-New_bt_Close =bt_Close_model.predict(LSTM_last_input)
-
-New_bt_Volume =bt_Volume_model.predict(LSTM_last_input)
-
-# New_bt_close_off_high =bt_close_off_high_model.predict(LSTM_last_input)
-
-New_bt_volatilitybt_close =bt_volatility_model.predict(LSTM_last_input)
-
-New_input[0][9][0] = New_eth_Close
-
-New_input[0][9][1] = New_eth_Volume
-
-New_input[0][9][2] = eth_close_off_high_model.predict(LSTM_last_input)
+# print(eth_Close_model.predict(New_input))
 
 
-New_input[0][9][3] = New_eth_volatilityeth
+# # # print(LSTM_test_inputs[-1])
+# # print("this is total test")
+# # print(LSTM_test_inputs)
+# # print("this is the total predict")
+# # print(eth_Close_model.predict(LSTM_test_inputs)) 
 
-New_input[0][9][4] = New_bt_Close
-
-New_input[0][9][5] = New_bt_Volume
-
-New_input[0][9][6] = bt_close_off_high_model.predict(LSTM_last_input)
-
-
-New_input[0][9][7] =New_bt_volatilitybt_close
-
-# print(New_input)
-print(eth_Close_model.predict(LSTM_test_inputs))
-
-
-# # print(LSTM_test_inputs[-1])
-# print("this is total test")
-# print(LSTM_test_inputs)
-# print("this is the total predict")
-# print(eth_Close_model.predict(LSTM_test_inputs)) 
-
-# print("this is the last test")
-# print(LSTM_last_input)
-# print("this is the last predict")
-# print(eth_Close_model.predict(LSTM_last_input))
+# # print("this is the last test")
+# # print(LSTM_last_input)
+# # print("this is the last predict")
+# # print(eth_Close_model.predict(LSTM_last_input))
 
 
 
-# predict_date=pd.read_csv('date.csv')
+# # predict_date=pd.read_csv('date.csv')
 
-# fig, ax1 = plt.subplots(1,1)
-# ax1.set_xticks([datetime.date(2018,i+1,1) for i in range(12)])
-# ax1.set_xticklabels([datetime.date(2018,i+1,1).strftime('%b %d %Y')  for i in range(12)])
-# # ax1.plot(model_data[model_data['Date']>= split_date]['Date'][window_len:].astype(datetime.datetime),
-# #          test_set['eth_Close'][window_len:], label='Actual')
-# ax1.plot(predict_date.tail(53)['date'].astype(datetime.datetime),
-#          ((np.transpose(eth_Close_model.predict(LSTM_test_inputs))+1) * test_set['eth_Close'].values[:-window_len])[0], 
-#          label='Predicted')
-# ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(eth_Close_model.predict(LSTM_test_inputs))+1)-\
-#             (test_set['eth_Close'].values[window_len:])/(test_set['eth_Close'].values[:-window_len]))), 
-#              xy=(0.75, 0.9),  xycoords='axes fraction',
-#             xytext=(0.75, 0.9), textcoords='axes fraction')
-# ax1.set_title('Test Set: Single Timepoint Prediction',fontsize=13)
-# ax1.set_ylabel('Ethereum Price ($)',fontsize=12)
-# ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
-# plt.show()
+# # fig, ax1 = plt.subplots(1,1)
+# # ax1.set_xticks([datetime.date(2018,i+1,1) for i in range(12)])
+# # ax1.set_xticklabels([datetime.date(2018,i+1,1).strftime('%b %d %Y')  for i in range(12)])
+# # # ax1.plot(model_data[model_data['Date']>= split_date]['Date'][window_len:].astype(datetime.datetime),
+# # #          test_set['eth_Close'][window_len:], label='Actual')
+# # ax1.plot(predict_date.tail(53)['date'].astype(datetime.datetime),
+# #          ((np.transpose(eth_Close_model.predict(LSTM_test_inputs))+1) * test_set['eth_Close'].values[:-window_len])[0], 
+# #          label='Predicted')
+# # ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(eth_Close_model.predict(LSTM_test_inputs))+1)-\
+# #             (test_set['eth_Close'].values[window_len:])/(test_set['eth_Close'].values[:-window_len]))), 
+# #              xy=(0.75, 0.9),  xycoords='axes fraction',
+# #             xytext=(0.75, 0.9), textcoords='axes fraction')
+# # ax1.set_title('Test Set: Single Timepoint Prediction',fontsize=13)
+# # ax1.set_ylabel('Ethereum Price ($)',fontsize=12)
+# # ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
+# # plt.show()
 
 
